25a6c1fd06fae8de573c91d99b6314c451319eab
Thomas_465 <160943727+Thomas465xd@users.noreply.github.com>
2024-08-17 21:48:30 -0400

    *GuitarLA React project in TypeScript - Migrated the base aplication of guitarLA made in React using js - Currently there are errors in some .tsx archives but those will be solved later

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.js'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+// Assertion not null or Non null assertion operator (!)
